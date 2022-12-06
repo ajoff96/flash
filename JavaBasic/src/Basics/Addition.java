867b74d6f5c07ca9ccb7772aa03085842d26dbbe
AJiTHUP96 <ajithoff96@gmail.com>
2022-12-06 15:00:21 +0530

    Trial

---
+++ JavaBasic/src/Basics/Addition.java
@@ -0,0 +1,15 @@
+package Basics;
+
+public class Addition {
+
+	public static void Add(int a, int b) {//argument
+		int c = a + b;
+		System.out.println(c);
+	}
+
+	public static void main(String[] args) {
+		Add(5, 10);//parameter passed by method block
+		
+	}
+
+}
